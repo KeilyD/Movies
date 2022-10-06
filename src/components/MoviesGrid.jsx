3b5d1037199f85e0747bc8ead888ba75bbb45522
Nicolás Roldán <you@example.com>
2022-10-06 15:01:06 -0300

    first commit

---
+++ src/components/MoviesGrid.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { get } from "../utils/httpClient"
+import { MovieCard } from "./../components/MovieCard";
+import styles from "./../components/MoviesGrid.module.css"
+
+export function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    get("/discover/movie").then((data) => {
+      setMovies(data.results);
+    });
+  }, []);
+  return (
+    <ul className={styles.moviesGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+}
